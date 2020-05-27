9577df41f9d7d69e728f5f395452cda7e0dc7e71
scherubin <62395373+scherubin@users.noreply.github.com>
2020-05-27 16:11:42 +0100

    part 1 of assignment 3

--- best.R
+++ best.R
@@ -1,4 +1,8 @@
 #reading in the outcome data
 outcome <- 
   read.csv("~/Coursera JHU course/Data/rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", colClasses="character")
-str(outcome)
+head(outcome)
+
+#creating a histogram of the 30-day deaths
+death30 <- as.numeric(outcome[,11])
+hist(death30)
